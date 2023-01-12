5153d34b2ba2ff411946fb4e3fec5eb983147af8
shrishti300 <88239226+shrishti300@users.noreply.github.com>
2023-01-12 17:32:35 +0530

    12

---
+++ jan/12.cpp
@@ -0,0 +1,31 @@
+/* 1519. Number of Nodes in the Sub-Tree With the Same Label */
+
+   vector<int>fun(vector<vector<int>>&adj,string &labels,int i,vector<int>&result){
+        vector<int>ans(26,0);
+        result[i]=1;
+        ans[labels[i]-'a']=1;
+        for(int j=0;j!=adj[i].size();j++)
+            if(!result[adj[i][j]]){
+                vector<int>tmp=fun(adj,labels,adj[i][j],result);
+                for(int k=0;k!=26;k++) ans[k]+=tmp[k]
+;           
+ }
+ result[i]=ans[labels[i]-'a'];
+ return ans;
+        
+    }
+
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+     vector<vector<int>>adj(n);
+     vector<int>result(n,0);
+     for(int i=0;i!=edges.size();i++){
+         adj[edges[i][0]].push_back(edges[i][1]);
+         adj[edges[i][1]].push_back(edges[i][0]);
+     } 
+     fun(adj,labels,0,result);
+     return result;  
+    }
+};
+
+// create an adjacent array 
+// do dfs traversal on each to find value
