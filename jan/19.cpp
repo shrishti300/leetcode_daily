42a588ba6b2c1b31498e43742f78cb5c6c9a0c8c
shrishti300 <88239226+shrishti300@users.noreply.github.com>
2023-01-20 22:23:51 +0530

    19th

---
+++ jan/19.cpp
@@ -0,0 +1,24 @@
+/* 974. Subarray Sums Divisible by K
+ */
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+          int n=nums.size();
+   int sum=0;
+   int result=0;
+   map<int,int>mp;
+   mp[0]=1;
+   for(int i=0;i<n;i++){
+       sum+=nums[i];
+       int rem=sum%k;
+       if(rem<0)
+       rem+=k;
+   if(mp.find(rem)!=mp.end()){
+       result+=mp[rem];}
+   mp[rem]++;
+    
+   }
+    return result;
+    }
+};
